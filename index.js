0456e072076f5f79ccd3e8f2734829781f23635b
EstherWanjiruUge <ugeesther@gmail.com>
2022-08-18 18:54:22 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(todo = "roller-skate") {
+  return `This Saturday, I want to ${todo}!`;
+}
+function mondayWork(todo = "go to the office") {
+  return `This Monday, I will ${todo}.`;
+}
+function wrapAdjective(flair = "*") {
+    const innerFunction = function (emphatic = "a hard worker") {
+        return `You are ${flair}${emphatic}${flair}!`;
+    };
+    return innerFunction;
+}
